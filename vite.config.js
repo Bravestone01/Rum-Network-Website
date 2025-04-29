70fcf33bc4cd6178431c1d6dd272d66180ef983c
mujtaba shah <Bravestone01>
2025-04-29 17:05:44 +0500

    sitemap issue solved again5

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ViteSitemap from 'vite-plugin-sitemap'
 
-
 export default defineConfig({
   plugins: [
     react(),
     ViteSitemap({
-      hostname: 'https://rum-network-website.vercel.app/', 
+      hostname: 'https://rum-network-website.vercel.app',
+      changefreq: 'daily',
+      priority: 1.0,
+      lastmod: new Date().toISOString(),
     })
   ],
 })
